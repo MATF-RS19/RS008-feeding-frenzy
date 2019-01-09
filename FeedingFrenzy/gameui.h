039bcc111a8c1bbeaaa670d42295b8e6e8e6f742
Aleksandra Radosavljevic <aleksandraradosavljevic@live.com>
2019-01-10 00:10:08 +0100

    Added sounds for upgrade
    For player to notice that he is upgraded.

--- FeedingFrenzy/gameui.h
+++ FeedingFrenzy/gameui.h
@@ -3,6 +3,7 @@
 
 #include "gamemodel.h"
 #include "ui_screencontroller.h"
+#include <QMediaPlayer>
 
 
 class GameUi {
@@ -10,6 +11,8 @@ public:
     GameUi(Ui::screencontroller* gameUi);
     void UpdateUi(GameModel* model);
 private:
+    int sign1,sign2,sign3;
+    QMediaPlayer *upgrade_sound;
     Ui::screencontroller* ui;
 };
 
