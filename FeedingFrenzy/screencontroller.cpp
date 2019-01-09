ea9bf0e5196b653a038527d755a6a83dbb0759dc
Luka Glavonjic (Home PC) <lukag@nordeus.com>
2019-01-09 23:37:56 +0100

    Arranged some ui

--- FeedingFrenzy/screencontroller.cpp
+++ FeedingFrenzy/screencontroller.cpp
@@ -37,13 +37,10 @@ void ScreenController::GoToGameOverScreen(bool hasWon, int score){
     ScreenController::SwitchScreen(GameOver);
     //Setup UI for game over
 
-
     if(hasWon){
         ui->result->setText("Congratulations, you won!");
-        ui->result->move(390, 110);
     } else {
         ui->result->setText("Sorry, you lost!");
-        ui->result->move(430, 110);
     }
     ui->yourscore->setText("Your score: " + QString::number(score));
 }
